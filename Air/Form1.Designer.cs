1915617552b966af5e061564c48369e272f8b3a7
Liu Yong Song <a15964852@gmail.com>
2018-12-19 23:59:20 +0800

    multithread ok

---
+++ Air/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Air
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(187, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(115, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ComSearch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(325, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PM10 : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "PM1 : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 12);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "PM2.5 : ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 12);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "PM10 : ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(115, 116);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM6";
+            this.groupBox1.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(187, 186);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 116);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "COM6";
+            this.groupBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "PM1 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "PM2.5 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "PM10 : ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(325, 186);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(115, 116);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "COM6";
+            this.groupBox3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "PM1 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "PM2.5 : ";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.label11);
+            this.groupBox4.Controls.Add(this.label12);
+            this.groupBox4.Location = new System.Drawing.Point(459, 186);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(115, 116);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "COM6";
+            this.groupBox4.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(20, 54);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 12);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "PM2.5 : ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(20, 80);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(42, 12);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "PM10 : ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(20, 29);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(36, 12);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "PM1 : ";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label14);
+            this.groupBox5.Controls.Add(this.label13);
+            this.groupBox5.Controls.Add(this.label15);
+            this.groupBox5.Location = new System.Drawing.Point(594, 186);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(115, 116);
+            this.groupBox5.TabIndex = 14;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "COM6";
+            this.groupBox5.Visible = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(20, 54);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(45, 12);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "PM2.5 : ";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(20, 80);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(42, 12);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "PM10 : ";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(614, 51);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(72, 12);
+            this.label16.TabIndex = 10;
+            this.label16.Text = "Not connected";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 12);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "PM1 : ";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label17.Location = new System.Drawing.Point(41, 125);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(140, 16);
+            this.label17.TabIndex = 15;
+            this.label17.Text = "2018/12/19 23:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 330);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
---
+++ Air/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Air
+{
+    public partial class Form1 : Form
+    {
+        private List<SerialPort> serial_port;
+        private List<String> comport_list;
+        private List<GroupBox> groupbox_list;
+        private List<Label> label_list;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetPortName()
+        {
+            comboBox1.Items.Clear();
+
+            string[] serialPorts = SerialPort.GetPortNames();
+            foreach (string serialPort in serialPorts)
+            {
+                comboBox1.Items.Add(serialPort);
+                if (comboBox1.Items.Count > 0)
+                {
+                    comboBox1.SelectedIndex = 0;
+                }
+            }
+        }
+        private void CreateComport()
+        {
+            SerialPort port;
+            foreach (String comport in comport_list)
+            {
+                port = new SerialPort(comport, 9600, Parity.None, 8, StopBits.One);
+                serial_port.Add(port);
+            }
+        }
+        private void OpenComport()
+        {
+            for (int i = 0; i < serial_port.Count; i++)
+            {
+                try
+                {
+                    if ((serial_port[i] != null) && (!serial_port[i].IsOpen))
+                    {
+                        serial_port[i].Open();
+                        ParameterizedThreadStart record_T = new ParameterizedThreadStart(record);
+                        Thread record_Thread = new Thread(record_T);
+                        Console.WriteLine("Already start thread ");
+                        record_Thread.Start(i);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(String.Format("出問題啦:{0}", e.ToString()));
+                }
+            }
+            label16.Text = "Connected";
+        }
+        private void record(object number)
+        {
+            int[] buffer = new int[24];
+            int index = 0;
+            int n = Convert.ToInt32(number);
+            String str = "";
+            while (true)
+            {
+                buffer[index] = serial_port[n].ReadByte();
+                index += 1;
+                if (index == 24)
+                {
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        str += Convert.ToString(buffer[i], 16) + " ";
+                    }
+                    label_list[n * 3].Text = "PM1 : " + buffer[5].ToString() + " ug/m3";
+                    label_list[n * 3 + 1].Text = "PM2.5 : " + buffer[7].ToString() + " ug/m3";
+                    label_list[n * 3 + 2].Text = "PM10 : " + buffer[9].ToString() + " ug/m3";
+                    label17.Text = (DateTime.Now.Year) + "/" + (DateTime.Now.Month) + "/" + (byte)(DateTime.Now.Day) + " " + (DateTime.Now.Hour) + ":" + (DateTime.Now.Minute) + ":" + (DateTime.Now.Second);
+                    /*Console.WriteLine(str);
+                    Console.WriteLine((DateTime.Now.Year) + "/" + (DateTime.Now.Month) + "/" + (byte)(DateTime.Now.Day) + " " + (DateTime.Now.Hour) + ":" + (DateTime.Now.Minute) + ":" + (DateTime.Now.Second));
+                    Console.WriteLine("PM1 (CF=1) = " + buffer[5]);
+                    Console.WriteLine("PM2.5 (CF=1) = " + buffer[7]);
+                    Console.WriteLine("PM10 (CF=1) = " + buffer[9]);
+                    Console.WriteLine("PM1 = " + buffer[11]);
+                    Console.WriteLine("PM2.5 = " + buffer[13]);
+                    Console.WriteLine("PM10 = " + buffer[15]);
+                    Console.WriteLine("--------------------------------------");*/
+
+                    //initial 
+                    str = "";
+                    Array.Clear(buffer, 0, buffer.Length);
+                    index = 0;
+                    
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Bug
+            Form1.CheckForIllegalCrossThreadCalls = false;
+
+            //initial
+            comport_list = new List<String>();
+            groupbox_list = new List<GroupBox>();
+            label_list = new List<Label>();
+            serial_port = new List<SerialPort>();
+
+            groupbox_list.Add(groupBox1);
+            groupbox_list.Add(groupBox2);
+            groupbox_list.Add(groupBox3);
+            groupbox_list.Add(groupBox4);
+            groupbox_list.Add(groupBox5);
+
+            label_list.Add(label1);
+            label_list.Add(label2);
+            label_list.Add(label3);
+            label_list.Add(label4);
+            label_list.Add(label5);
+            label_list.Add(label6);
+            label_list.Add(label7);
+            label_list.Add(label8);
+            label_list.Add(label9);
+            label_list.Add(label10);
+            label_list.Add(label11);
+            label_list.Add(label12);
+            label_list.Add(label13);
+            label_list.Add(label14);
+            label_list.Add(label15);
+
+
+            GetPortName();
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comport_list.Add((String)comboBox1.SelectedItem);
+            for (int i = 0; i < comport_list.Count; i++)
+            {
+                groupbox_list[i].Text = comport_list[i];
+                groupbox_list[i].Visible = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetPortName();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CreateComport();
+            OpenComport();
+        }
+    }
+}
